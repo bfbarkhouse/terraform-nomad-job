f3f73331c6e279a56c9599f16296923fa585d7f7
bbarkhouse-hashicorp <84409718+bbarkhouse-hashicorp@users.noreply.github.com>
2024-01-27 21:49:38 -0500

    comments

--- main.tf
+++ main.tf
@@ -11,5 +11,6 @@ data "vault_nomad_access_token" "vault_nomad_token" {
 }
 
 resource "nomad_job" "hello-world-job-vault-dynamic-creds" {
-  jobspec = file(var.nomad_job_spec_file)
+  jobspec = file(var.nomad_job_spec_file) #this function works only with files that are distributed as part of the configuration source code, so if this file will be created by a resource in this configuration you must instead obtain this result from an attribute of that resource.
+  deregister_on_destroy = true
 }
