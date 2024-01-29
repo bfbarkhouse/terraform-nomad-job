e7cf08f29fae7e9b6517726e6ae212076d6bdffc
bbarkhouse-hashicorp <84409718+bbarkhouse-hashicorp@users.noreply.github.com>
2024-01-29 11:39:06 -0500

    fixed typo

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "nomad_secrets_role" {
   type = string
-  description = "Name of the Nomad secrets engine role used to submit the job. Note: If your you Vault identity doesn't not have permissions to the role, the run will fail."
+  description = "Name of the Nomad secrets engine role used to submit the job. Note: If your Vault identity doesn't not have permissions to the role, the run will fail."
 }
 variable "nomad_job_spec_file" {
   type        = string
